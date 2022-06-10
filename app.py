d74789e572fdd71893c81861d8da5990c3628a82
danielpartida <daniel.partida@d-fine.de>
2022-06-10 15:09:34 +0200

    testing

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def hello_world():
+    return 'Hello, Prashant!'
+
+    
+
+app = Flask(__name__)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
