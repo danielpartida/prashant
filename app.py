4445323c9cfc9e2444090671838cf833252b07c4
danielpartida <daniel.partida@d-fine.de>
2022-06-10 14:27:19 +0200

    adding first files to repo

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+server = app.server
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, Prashant!'
+
+if __name__ == "__main__":
+     app.run(host="0.0.0.0")
